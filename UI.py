e8cb6eb85a4a324d063e085dc99a471b01b12930
TaVo <71878292+avoutsinas@users.noreply.github.com>
2021-08-01 04:55:20 +0100

    Changes to UI

--- UI.py
+++ UI.py
@@ -1,34 +1,108 @@
 import tkinter as tk
+from tkinter import simpledialog
+from tkinter import *
+import numpy as np
+from Preliminaries import *
+from FA import *
+from Algorithms import *
 
 
-class App(tk.Tk):
+class App(Frame):
     radius = 20
 
     def __init__(self):
         super().__init__()
-        self.canvas = tk.Canvas(self, width=500, height=500, bg='beige')
-        self.canvas.pack()
+        self.pack(expand=Y, fill=BOTH)
 
-        self.canvas.bind('<1>', self.select_circle)
-        self.canvas.bind('<Shift-1>', self.make_circle)
+        self.main_canvas = Canvas(self, width=1400, height=850, bg='#00ffff')
+        self.main_canvas.pack(expand=Y, fill=BOTH)
+
+        self.input_canvas = tk.Canvas(self.main_canvas, width=1000, height=370, bg='white')
+        self.input_canvas.pack()
+
+        self.output_canvas = tk.Canvas(self.main_canvas, width=1000, height=370, bg='white')
+        self.output_canvas.pack()
+
+        self.input_window = tk.Canvas(self.main_canvas, width=350, height=370, bg='white')
+        self.input_window.pack()
+
+        self.output_window = tk.Canvas(self.main_canvas, width=350, height=370, bg='white')
+        self.output_window.pack()
+
+        self.setup()
 
         self.selected = None
+        self.start_flag = False
 
-    def make_circle(self, event):
-        x, y, r = event.x, event.y, self.radius
-        self.canvas.create_oval(x-r, y-r, x+r, y+r, outline='black', fill='white')
+        self.states = []
+        self.transitions = []
+
+    def setup(self):
+        self.main_canvas.create_window(390, 35, anchor=NW, window=self.input_canvas)
+        self.main_canvas.create_window(390, 420, anchor=NW, window=self.output_canvas)
+        self.main_canvas.create_window(25, 35, anchor=NW, window=self.input_window)
+        self.main_canvas.create_window(25, 420, anchor=NW, window=self.output_window)
+
+        self.input_canvas.bind('<1>', self.select_circle)
+        self.input_canvas.bind('<Shift-1>', self.create_state)
+        # self.input_canvas.bind('<2>', self.make_arc)
+
+    def create_state(self, event):
+
+        state_name = simpledialog.askstring(title="State Creation", prompt="Enter the name of the State",
+                                            parent=self.input_canvas)
+        while state_name == "":
+            state_name = simpledialog.askstring(title="State Creation", prompt="States must not have empty names",
+                                                parent=self.input_canvas)
+
+        if state_name is not None:
+            x, y, r = event.x, event.y, self.radius
+            if self.states == []:
+                new_state = State(state_name, True, False)
+
+                self.input_canvas.create_line(x - r, y, x - 2.2 * r, y, arrow=tk.FIRST, tags="arrow")
+                self.input_canvas.create_oval(x - r, y - r, x + r, y + r, outline='black', fill='beige',
+                                              tags=(state_name, "first"))
+                self.input_canvas.create_text(x, y, text=state_name, fill="black", tags=(state_name, "first", "label"))
+            else:
+                new_state = State(state_name, False, False)
+
+                self.input_canvas.create_oval(x - r, y - r, x + r, y + r, outline='black', fill='beige',
+                                              tags=state_name)
+                self.input_canvas.create_text(x, y, text=state_name, fill="black", tags=(state_name, "label"))
+
+        self.states.append(new_state)
+        print(self.states)
 
     def select_circle(self, event):
-        self.canvas.bind('<Motion>', self.move_circle)
-        self.canvas.bind('<ButtonRelease-1>', self.deselect)
+        x, y = event.x, event.y
+        self.input_canvas.bind('<Motion>', self.move_state)
+        self.input_canvas.bind('<ButtonRelease-1>', self.deselect)
 
-        self.canvas.addtag_withtag('selected', tk.CURRENT)
+        if "label" not in self.input_canvas.gettags(tk.CURRENT):
+            self.input_canvas.addtag_withtag('selected', tk.CURRENT)
+            self.input_canvas.addtag_closest('selected_txt', x, y, halo=13)
+            print(self.input_canvas.gettags(tk.CURRENT))
+        elif "label" in self.input_canvas.gettags(tk.CURRENT):
+            self.input_canvas.addtag_withtag('selected_txt', tk.CURRENT)
+            self.input_canvas.addtag_closest('selected', x, y, halo=13, start=tk.CURRENT)
+            print(self.input_canvas.gettags(tk.CURRENT))
 
-    def move_circle(self, event):
+        if "first" in self.input_canvas.gettags(tk.CURRENT):
+            self.start_flag = True
+            self.input_canvas.addtag_withtag('selected_arrow', "arrow")
+        else:
+            self.start_flag = False
+
+    def move_state(self, event):
         x, y, r = event.x, event.y, self.radius
-        self.canvas.coords('selected', x-r, y-r, x+r, y+r)
+        self.input_canvas.coords('selected', x - r, y - r, x + r, y + r)
+        self.input_canvas.coords('selected_txt', x, y)
+        if self.start_flag:
+            self.input_canvas.coords("selected_arrow", x - r, y, x - 2.2 * r, y)
 
     def deselect(self, event):
-        self.canvas.dtag('selected')    # removes the 'selected' tag
-        self.canvas.unbind('<Motion>')
-        self.canvas.bind('<Shift-1>', self.make_circle)
+        self.input_canvas.dtag('selected')  # removes the 'selected' tag
+        self.input_canvas.dtag('selected_txt')
+        self.input_canvas.unbind('<Motion>')
+        self.input_canvas.bind('<Shift-1>', self.create_state)
--- main.py
+++ main.py
@@ -157,5 +157,5 @@ def main():
 
 
 if __name__ == "__main__":
-    # App().mainloop()
+    App().mainloop()
     main()
