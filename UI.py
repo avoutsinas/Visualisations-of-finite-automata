903bcc6cc497249b39a2e586b9ad8c423144a153
TaVo <71878292+avoutsinas@users.noreply.github.com>
2021-08-01 23:48:41 +0100

    Dynamic Arrows trial

--- UI.py
+++ UI.py
@@ -48,6 +48,7 @@ class App(Frame):
 
         self.states = []
         self.transitions = []
+        self.found_state = []
 
     def setup(self):
         self.main_canvas.create_window(390, 35, anchor=NW, window=self.input_canvas)
@@ -78,7 +79,7 @@ class App(Frame):
                                             parent=self.input_canvas)
 
         if state_name is not None and final is not None:
-            x, y, r, r2 = event.x, event.y, self.radius, self.radius - 2
+            x, y, r, r2 = event.x, event.y, self.radius, self.radius - 3
 
             if not self.states:
                 self.input_canvas.create_line(x - r, y, x - 2.2 * r, y, arrow=tk.FIRST, tags=(state_name, "arrow"))
@@ -110,41 +111,98 @@ class App(Frame):
         x, y = event.x, event.y
         self.input_canvas.bind('<Motion>', self.move_state)
         self.input_canvas.bind('<ButtonRelease-1>', self.deselect)
+        self.input_canvas.bind('<Control-1>', self.create_transition)
+
         tags = self.input_canvas.gettags(tk.CURRENT)
         name_tag = tags[0]
         print(name_tag)
 
-        if "label" in self.input_canvas.gettags(tk.CURRENT):
+        if "label" in tags:
             self.input_canvas.addtag_withtag('selected_txt', tk.CURRENT)
             self.input_canvas.addtag_closest('selected', x, y, halo=13, start=tk.CURRENT)
             self.input_canvas.addtag_withtag("selected_final", name_tag + "final")
+            self.input_canvas.addtag_withtag("selected_exit_transition", "from" + name_tag)
+            self.input_canvas.addtag_withtag("selected_entry_transition", "to" + name_tag)
             # print(self.input_canvas.gettags(tk.CURRENT))
 
             if "first" in self.input_canvas.gettags(tk.CURRENT):
-                self.start_flag = True
                 self.input_canvas.addtag_withtag('selected_arrow', "arrow")
 
     def move_state(self, event):
-        x, y, r, r2 = event.x, event.y, self.radius, self.radius - 2
+        x, y, r, r2 = event.x, event.y, self.radius, self.radius - 3
 
-        self.input_canvas.coords('selected', x - r, y - r, x + r, y + r)
-        self.input_canvas.coords('selected_txt', x, y)
-        self.input_canvas.coords("selected_final", x - r2, y - r2, x + r2, y + r2)
-        if "first" in self.input_canvas.gettags(tk.CURRENT):
-            self.input_canvas.coords("selected_arrow", x - r, y, x - 2.2 * r, y)
+        tags = self.input_canvas.gettags(tk.CURRENT)
+        name_tag = tags[0]
+
+        if "label" in tags:
+            self.input_canvas.coords('selected', x - r, y - r, x + r, y + r)
+            self.input_canvas.coords('selected_txt', x, y)
+            self.input_canvas.coords("selected_final", x - r2, y - r2, x + r2, y + r2)
+            if "first" in self.input_canvas.gettags(tk.CURRENT) and "label" in tags:
+                self.input_canvas.coords("selected_arrow", x - r, y, x - 2.2 * r, y)
+
+            exit_arrows = self.input_canvas.find_withtag("from" + name_tag)
+            print(exit_arrows)
+            exit = self.input_canvas.coords("selected_exit_transition")
+            if len(exit) > 0:
+                x1, y1, x2, y2 = self.input_canvas.coords("selected_exit_transition")
+                self.input_canvas.coords("selected_exit_transition", x + r, y, x2, y2)
+
+            entry = self.input_canvas.coords("selected_entry_transition")
+            if len(entry) > 0:
+                x3, y3, x4, y4 = entry
+                self.input_canvas.coords("selected_entry_transition", x3, y3, x - r, y)
 
     def create_transition(self, event):
         x, y, r = event.x, event.y, self.radius
+        tags = self.input_canvas.gettags(tk.CURRENT)
+        name_tag = tags[0]
+        print(name_tag)
 
-        
+        if len(self.found_state) < 2:
+            if self.found_state == [] and name_tag in [i.get_name() for i in self.states]:
+                if "label" in tags:
+                    self.input_canvas.addtag_withtag('start_point', tk.CURRENT)
+                    self.start_x = x
+                    self.start_y = y
+                    self.found_state.append(name_tag)
+                    print("found starting point")
+                    print(tags)
+                    print(self.found_state)
+
+            if len(self.found_state) == 1 and name_tag in [i.get_name() for i in self.states]:
+                if name_tag != self.found_state[0]:
+                    if "label" in tags:
+                        self.input_canvas.addtag_withtag('end_point', tk.CURRENT)
+                        self.end_x = x
+                        self.end_y = y
+                        self.found_state.append(name_tag)
+                        print("found end point")
+                        print(tags)
+                        print(self.found_state)
+
+                        if self.start_x < self.end_x:
+                            self.input_canvas.create_line(self.start_x + r, self.start_y, self.end_x - r, self.end_y,
+                                                          arrow=tk.LAST, tags=(
+                                    "from" + self.found_state[0], "to" + self.found_state[1],
+                                    "transition"))
+                        else:
+                            self.input_canvas.create_line(self.start_x - r, self.start_y, self.end_x + r, self.end_y,
+                                                          arrow=tk.LAST, tags=(
+                                    "from" + self.found_state[0], "to" + self.found_state[1],
+                                    "transition"))
+
+                        self.found_state = []
 
     def deselect(self, event):
         self.input_canvas.dtag('selected')  # removes the 'selected' tag
         self.input_canvas.dtag('selected_txt')
         self.input_canvas.dtag("selected_final")
+        self.input_canvas.dtag("selected_exit_transition")
+        self.input_canvas.dtag("selected_entry_transition")
         self.input_canvas.unbind('<Motion>')
         self.input_canvas.bind('<Shift-1>', self.create_state)
 
     def clear_input(self, event):
-        self.input_canvas.delete("circle", "arrow", "label")
+        self.input_canvas.delete("circle", "arrow", "label", "transition")
         self.states = []
