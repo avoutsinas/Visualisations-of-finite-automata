420af556ab53d58cf632b4292c5362cbba6d392c
TaVo <71878292+avoutsinas@users.noreply.github.com>
2021-08-02 05:21:38 +0100

    added letters to transitions, needs more work

--- UI.py
+++ UI.py
@@ -48,7 +48,7 @@ class App(Frame):
 
         self.states = []
         self.transitions = []
-        self.found_state = []
+        self.transition_states = []
 
     def setup(self):
         self.main_canvas.create_window(390, 35, anchor=NW, window=self.input_canvas)
@@ -151,39 +151,58 @@ class App(Frame):
 
     def move_state_transitions(self, name_tag, x, y, r):
         exit_arrows = self.input_canvas.find_withtag("from" + name_tag)
-        print(exit_arrows)
+        exit_arrow_text = self.input_canvas.find_withtag("textfrom" + name_tag)
+
         for item in exit_arrows:
             exiting = self.input_canvas.coords(item)
-            # print("exiting arrow: " + str(exiting))
+
             x1, y1, x2, y2, x3, y3 = exiting
             midx = (x + x3) / 2
+            midy1 = (y + y3) / 2 + np.abs(x - x3) / 4
+            midy2 = (y + y3) / 2 - np.abs(x - x3) / 4
 
             if "1" in self.input_canvas.gettags(item):
                 if x >= x2:
-                    midy = (y + y3) / 2 + np.abs(x - x3) / 4
-                    self.input_canvas.coords(item, x - r, y + r / 2, midx, midy, x3, y3)
+                    self.input_canvas.coords(item, x - r, y + r / 2, midx, midy1, x3, y3)
+                    for text in exit_arrow_text:
+                        if "1" in self.input_canvas.gettags(text):
+                            self.input_canvas.coords(text, midx, midy1)
                 else:
-                    midy = (y + y3) / 2 - np.abs(x - x3) / 4
-                    self.input_canvas.coords(item, x + r, y - r / 2, midx, midy, x3, y3)
+                    self.input_canvas.coords(item, x + r, y - r / 2, midx, midy2, x3, y3)
+                    for text in exit_arrow_text:
+                        if "1" in self.input_canvas.gettags(text):
+                            self.input_canvas.coords(text, midx, midy2)
+
             elif "2" in self.input_canvas.gettags(item):
                 if x >= x2:
-                    midy = (y + y3) / 2 + np.abs(x - x3) / 4
-                    self.input_canvas.coords(item, x - r, y + r / 2, midx, midy, x3, y3)
+                    self.input_canvas.coords(item, x - r, y + r / 2, midx, midy1, x3, y3)
+                    for text in exit_arrow_text:
+                        if "2" in self.input_canvas.gettags(text):
+                            self.input_canvas.coords(text, midx, midy1)
                 else:
-                    midy = (y + y3) / 2 - np.abs(x - x3) / 4
-                    self.input_canvas.coords(item, x + r, y - r / 2, midx, midy, x3, y3)
+                    self.input_canvas.coords(item, x + r, y - r / 2, midx, midy2, x3, y3)
+                    for text in exit_arrow_text:
+                        if "2" in self.input_canvas.gettags(text):
+                            self.input_canvas.coords(text, midx, midy2)
 
         entry_arrows = self.input_canvas.find_withtag("to" + name_tag)
+        entry_arrow_text = self.input_canvas.find_withtag("textto" + name_tag)
         for item in entry_arrows:
             entry = self.input_canvas.coords(item)
+
             x4, y4, x5, y5, x6, y6 = entry
             midx = (x4 + x) / 2
+            midy1 = (y + y4) / 2 - np.abs(x - x4) / 4
+            midy2 = (y + y4) / 2 + np.abs(x - x4) / 4
+
             if x >= x4:
-                midy = (y + y4) / 2 - np.abs(x - x4) / 4
-                self.input_canvas.coords(item, x4, y4, midx, midy, x - r, y - r / 2)
+                self.input_canvas.coords(item, x4, y4, midx, midy1, x - r, y - r / 2)
+                for text in entry_arrow_text:
+                    self.input_canvas.coords(text, midx, midy1)
             else:
-                midy = (y + y4) / 2 + np.abs(x - x4) / 4
-                self.input_canvas.coords(item, x4, y4, midx, midy, x + r, y + r / 2)
+                self.input_canvas.coords(item, x4, y4, midx, midy2, x + r, y + r / 2)
+                for text in entry_arrow_text:
+                    self.input_canvas.coords(text, midx, midy2)
 
     def create_transition(self, event):
         x, y, r = event.x, event.y, self.radius
@@ -191,39 +210,39 @@ class App(Frame):
         name_tag = tags[0]
         print(name_tag)
 
-        if len(self.found_state) < 2:
-            if self.found_state == [] and name_tag in [i.get_name() for i in self.states]:
+        if len(self.transition_states) < 2:
+            if self.transition_states == [] and name_tag in [i.get_name() for i in self.states]:
                 if "label" in tags:
                     self.input_canvas.addtag_withtag('start_point', tk.CURRENT)
                     self.start_x = x
                     self.start_y = y
-                    self.found_state.append(name_tag)
+                    self.transition_states.append(name_tag)
                     print("found starting point")
                     print(tags)
-                    print(self.found_state)
+                    print(self.transition_states)
 
-            if len(self.found_state) == 1 and name_tag in [i.get_name() for i in self.states]:
-                if name_tag != self.found_state[0]:
+            if len(self.transition_states) == 1 and name_tag in [i.get_name() for i in self.states]:
+                if name_tag != self.transition_states[0]:
                     if "label" in tags:
                         self.input_canvas.addtag_withtag('end_point', tk.CURRENT)
                         self.end_x = x
                         self.end_y = y
-                        self.found_state.append(name_tag)
+                        self.transition_states.append(name_tag)
                         print("found end point")
                         print(tags)
-                        print(self.found_state)
+                        print(self.transition_states)
 
                         self.draw_arrow(r)
-                        self.found_state = []
-                elif name_tag == self.found_state[0]:
-                    tag_to_add = ("self" + self.found_state[0], "transition", "3")
+                        self.transition_states = []
+                elif name_tag == self.transition_states[0]:
+                    tag_to_add = ("self" + self.transition_states[0], "transition", "3")
                     # self.input_canvas.create_line()
             else:
-                self.found_state = []
+                self.transition_states = []
 
     def draw_arrow(self, r):
         if self.start_x < self.end_x:
-            tag_to_add = ("from" + self.found_state[0], "to" + self.found_state[1], "transition", "1")
+            tag_to_add = ("from" + self.transition_states[0], "to" + self.transition_states[1], "transition", "1")
             """
             self.input_canvas.create_line(self.start_x + r, self.start_y - r / 2, self.end_x - r, self.end_y - r / 2,
                                           arrow=tk.LAST, tags=tag_to_add, fill="white") """
@@ -232,10 +251,23 @@ class App(Frame):
             midy = (self.start_y + self.end_y) / 2 - np.abs(self.start_x - self.end_x) / 4
 
             points = ((self.start_x + r, self.start_y - r / 2), (midx, midy), (self.end_x - r, self.end_y - r / 2))
-            self.input_canvas.create_line(points, arrow='last', smooth=1, fill="white", tags=tag_to_add, width=1.45)
+
+            transition_letters = simpledialog.askstring(title="Transition Creation",
+                                                        prompt="Specify the letter or letters that are used in this "
+                                                               "transition.\n\n Multiple letters should be seperated "
+                                                               "by commas.",
+                                                        parent=self.input_canvas)
+
+            if transition_letters not in ["", None]:
+                self.input_canvas.create_text(midx, midy - 1, text=transition_letters, fill="white", tags=(
+                    transition_letters, "text", "textfrom" + self.transition_states[0], "textto" + self.transition_states[1], "1"))
+
+                self.input_canvas.create_line(points, arrow='last', smooth=1, fill="white", tags=tag_to_add, width=1.45)
+            else:
+                self.transition_states = []
 
         elif self.start_x > self.end_x:
-            tag_to_add = ("from" + self.found_state[0], "to" + self.found_state[1], "transition", "2")
+            tag_to_add = ("from" + self.transition_states[0], "to" + self.transition_states[1], "transition", "2")
             """
             self.input_canvas.create_line(self.start_x - r, self.start_y + r / 2, self.end_x + r, self.end_y + r / 2,
                                           arrow=tk.LAST, tags=tag_to_add, fill="white") """
@@ -244,7 +276,20 @@ class App(Frame):
             midy = (self.start_y + self.end_y) / 2 + np.abs(self.start_x - self.end_x) / 4
 
             points = ((self.start_x - r, self.start_y + r / 2), (midx, midy), (self.end_x + r, self.end_y + r / 2))
-            self.input_canvas.create_line(points, arrow='last', smooth=1, fill="white", tags=tag_to_add, width=1.45)
+
+            transition_letters = simpledialog.askstring(title="Transition Creation",
+                                                        prompt="Specify the letter or letters that are used in this "
+                                                               "transition.\n\n Multiple letters should be seperated "
+                                                               "by commas.",
+                                                        parent=self.input_canvas)
+
+            if transition_letters not in ["", None]:
+                self.input_canvas.create_text(midx, midy + 1, text=transition_letters, fill="white", tags=(
+                    transition_letters,"text", "textfrom" + self.transition_states[0], "textto" + self.transition_states[1], "2"))
+
+                self.input_canvas.create_line(points, arrow='last', smooth=1, fill="white", tags=tag_to_add, width=1.45)
+            else:
+                self.transition_states = []
 
     def deselect(self, event):
         self.input_canvas.dtag('selected')  # removes the 'selected' tag
@@ -256,5 +301,5 @@ class App(Frame):
         self.input_canvas.bind('<Shift-1>', self.create_state)
 
     def clear_input(self, event):
-        self.input_canvas.delete("circle", "arrow", "label", "transition")
+        self.input_canvas.delete("circle", "arrow", "label", "transition","text")
         self.states = []
