9dd002b35cfc015a742651d545a6d068a704e94c
TaVo <71878292+avoutsinas@users.noreply.github.com>
2021-08-07 13:30:27 +0100

    Dynamic Scrollregions added

--- UI.py
+++ UI.py
@@ -10,14 +10,16 @@ from FA import *
 from Algorithms import *
 
 
-class Window:
+class Board:
     def __init__(self, main, width, height, bg, highlightthickness, highlightbackground, highlightcolor):
+        self.radius = main.radius
         self.container = tk.Frame(main, width=width, height=height)
         self.canvas = tk.Canvas(self.container, width=width, height=height, bg=bg,
                                 highlightthickness=highlightthickness,
                                 highlightbackground=highlightbackground, highlightcolor=highlightcolor,
-                                scrollregion=(0, 0, 1000, 1000))
+                                scrollregion=(0, 0, 1.4 * width, 2 * height))
 
+        """
         self.y_scrollbar = tk.Scrollbar(self.container, orient="vertical", command=self.canvas.yview, jump=1,
                                         width=12, troughcolor="black")
         self.scrollable_y_frame = tk.Frame(self.canvas)
@@ -31,23 +33,135 @@ class Window:
         self.canvas.create_window((0, 0), window=self.scrollable_x_frame, anchor="nw")
         self.canvas.configure(xscrollcommand=self.x_scrollbar.set)
         self.x_scrollbar.pack(side="bottom", fill="x")
+        """
 
-        self.width = width
-        self.height = height
-        self.font = tkFont.Font(family="consolas", size=14)
+    def select_state(self, event):
+        x, y = event.x, event.y
+        event.widget.bind('<Motion>', self.move_state)
+        event.widget.bind('<ButtonRelease-1>', self.deselect)
 
-    def show_pos(self, event):
+        tags = event.widget.gettags(tk.CURRENT)
+        name_tag = tags[0]
+        print(name_tag)
         print(event.x, event.y)
+        print(event.widget)
+
+        if "label" in tags:
+            event.widget.addtag_withtag('selected_txt', tk.CURRENT)
+            event.widget.addtag_closest('selected', x, y, halo=13, start=tk.CURRENT)
+            event.widget.addtag_withtag("selected_final", name_tag + "final")
+            event.widget.addtag_withtag("selected_self_transition", "self" + name_tag)
+            event.widget.addtag_withtag("selected_self_transition_text", "self" + name_tag + "text")
+
+            if "first" in event.widget.gettags(tk.CURRENT):
+                event.widget.addtag_withtag('selected_arrow', "arrow")
+
+    def deselect(self, event):
+        event.widget.dtag('selected')  # removes the 'selected' tag
+        event.widget.dtag('selected_txt')
+        event.widget.dtag("selected_final")
+        event.widget.dtag("selected_self_transition")
+        event.widget.dtag("selected_self_transition_text")
+        event.widget.unbind('<Motion>')
+
+    def move_state(self, event):
+        x, y, r, r2 = event.x, event.y, self.radius, self.radius - 3
+
+        tags = event.widget.gettags(tk.CURRENT)
+        name_tag = tags[0]
+
+        if "label" in tags:
+            event.widget.coords('selected', x - r, y - r, x + r, y + r)
+            event.widget.coords('selected_txt', x, y)
+            event.widget.coords("selected_final", x - r2, y - r2, x + r2, y + r2)
+
+            # coords for self-transitions
+            x1 = x - 0.9 * r
+            y1 = y - 0.5 * r
+            x2 = x + 0.9 * r
+            midx = (x1 + x2) / 2
+            midy = (y1 - 3 * r)
+
+            event.widget.coords("selected_self_transition", x1, y1, midx, midy, x2, y1)
+            event.widget.coords("selected_self_transition_text", midx, midy + 15)
+            if "first" in tags:
+                event.widget.coords("selected_arrow", x - r, y, x - 2.2 * r, y)
+
+            self.move_state_transitions(event, name_tag, x, y, r)
+
+    def move_state_transitions(self, event, name_tag, x, y, r):
+        self.move_outbound_transitions(event, name_tag, x, y, r)
+        self.move_inbound_transitions(event, name_tag, x, y, r)
+
+    @staticmethod
+    def move_outbound_transitions(event, name_tag, x, y, r):
+        exit_arrows = event.widget.find_withtag("from_" + name_tag)
+
+        for item in exit_arrows:
+            item_tags = event.widget.gettags(item)
+            text_tag = str((*item_tags, "text"))
+            text = event.widget.find_withtag(text_tag)
+
+            exiting = event.widget.coords(item)
+            x1, y1, x2, y2, x3, y3 = exiting
+            midx1 = (x - r + x3) / 2
+            midx2 = (x + r + x3) / 2
+            midy1 = (y + y3) / 2 + np.abs(x - x3) / 6
+            midy2 = (y + y3) / 2 - np.abs(x - x3) / 6
+            exit_coords, exit_coords_txt = None, None
+
+            if "1" in event.widget.gettags(item):
+                if x >= x2:
+                    exit_coords = (x - r, y + r / 2, midx1, midy1, x3, y3)
+                    exit_coords_txt = (midx1, midy1)
+                else:
+                    exit_coords = (x + r, y - r / 2, midx2, midy2, x3, y3)
+                    exit_coords_txt = (midx2, midy2)
+
+            elif "2" in event.widget.gettags(item):
+                if x >= x2:
+                    exit_coords = (x - r, y + r / 2, midx1, midy1, x3, y3)
+                    exit_coords_txt = (midx1, midy1)
+                else:
+                    exit_coords = (x + r, y - r / 2, midx2, midy2, x3, y3)
+                    exit_coords_txt = (midx2, midy2)
+
+            event.widget.coords(item, *exit_coords)
+            event.widget.coords(text, *exit_coords_txt)
+
+    @staticmethod
+    def move_inbound_transitions(event, name_tag, x, y, r):
+        entry_arrows = event.widget.find_withtag("to_" + name_tag)
+
+        for item in entry_arrows:
+            entry = event.widget.coords(item)
+            item_tags = event.widget.gettags(item)
+            text_tag = str((*item_tags, "text"))
+            text = event.widget.find_withtag(text_tag)
+
+            x4, y4, x5, y5, x6, y6 = entry
+            midx1 = (x4 + x - r) / 2
+            midx2 = (x4 + x + r) / 2
+            midy1 = (y + y4) / 2 - np.abs(x - x4) / 6
+            midy2 = (y + y4) / 2 + np.abs(x - x4) / 6
+
+            if x >= x4:
+                entry_coords = (x4, y4, midx1, midy1, x - r, y - r / 2)
+                entry_coords_txt = (midx1, midy1)
+            else:
+                entry_coords = (x4, y4, midx2, midy2, x + r, y + r / 2)
+                entry_coords_txt = (midx2, midy2)
+
+            event.widget.coords(item, *entry_coords)
+            event.widget.coords(text, *entry_coords_txt)
 
 
-class InputBoard:
+class InputBoard(Board):
     def __init__(self, main, width, height, bg, highlightthickness, highlightbackground, highlightcolor):
+        super().__init__(main, width, height, bg, highlightthickness, highlightbackground, highlightcolor)
 
         self.main = main
         self.start_x, self.start_y, self.end_x, self.end_y = 0, 0, 0, 0
-        self.canvas = tk.Canvas(main, width=width, height=height, bg=bg, highlightthickness=highlightthickness,
-                                highlightbackground=highlightbackground, highlightcolor=highlightcolor)
-
         self.radius = self.main.radius
         self.states = []
         self.transitions = []
@@ -198,125 +312,6 @@ class InputBoard:
                 event.widget.create_line(points, arrow='last', smooth=1, fill="white", tags=tag_to_add, width=1.45)
                 self.record_transition(transition_letters, same=False)
 
-    def select_state(self, event):
-        x, y = event.x, event.y
-        event.widget.bind('<Motion>', self.move_state)
-        event.widget.bind('<ButtonRelease-1>', self.deselect)
-
-        tags = event.widget.gettags(tk.CURRENT)
-        name_tag = tags[0]
-        print(name_tag)
-
-        if "label" in tags:
-            event.widget.addtag_withtag('selected_txt', tk.CURRENT)
-            event.widget.addtag_closest('selected', x, y, halo=13, start=tk.CURRENT)
-            event.widget.addtag_withtag("selected_final", name_tag + "final")
-            event.widget.addtag_withtag("selected_self_transition", "self" + name_tag)
-            event.widget.addtag_withtag("selected_self_transition_text", "self" + name_tag + "text")
-
-            if "first" in event.widget.gettags(tk.CURRENT):
-                event.widget.addtag_withtag('selected_arrow', "arrow")
-
-    def deselect(self, event):
-        event.widget.dtag('selected')  # removes the 'selected' tag
-        event.widget.dtag('selected_txt')
-        event.widget.dtag("selected_final")
-        event.widget.dtag("selected_self_transition")
-        event.widget.dtag("selected_self_transition_text")
-        event.widget.unbind('<Motion>')
-        event.widget.bind('<Shift-1>', self.create_state)
-
-    def move_state(self, event):
-        x, y, r, r2 = event.x, event.y, self.radius, self.radius - 3
-
-        tags = event.widget.gettags(tk.CURRENT)
-        name_tag = tags[0]
-
-        if "label" in tags:
-            event.widget.coords('selected', x - r, y - r, x + r, y + r)
-            event.widget.coords('selected_txt', x, y)
-            event.widget.coords("selected_final", x - r2, y - r2, x + r2, y + r2)
-
-            # coords for self-transitions
-            x1 = x - 0.9 * r
-            y1 = y - 0.5 * r
-            x2 = x + 0.9 * r
-            midx = (x1 + x2) / 2
-            midy = (y1 - 3 * r)
-
-            event.widget.coords("selected_self_transition", x1, y1, midx, midy, x2, y1)
-            event.widget.coords("selected_self_transition_text", midx, midy + 15)
-            if "first" in tags:
-                event.widget.coords("selected_arrow", x - r, y, x - 2.2 * r, y)
-
-            self.move_state_transitions(event, name_tag, x, y, r)
-
-    def move_state_transitions(self, event, name_tag, x, y, r):
-        self.move_outbound_transitions(event, name_tag, x, y, r)
-        self.move_inbound_transitions(event, name_tag, x, y, r)
-
-    @staticmethod
-    def move_outbound_transitions(event, name_tag, x, y, r):
-        exit_arrows = event.widget.find_withtag("from_" + name_tag)
-
-        for item in exit_arrows:
-            item_tags = event.widget.gettags(item)
-            text_tag = str((*item_tags, "text"))
-            text = event.widget.find_withtag(text_tag)
-
-            exiting = event.widget.coords(item)
-            x1, y1, x2, y2, x3, y3 = exiting
-            midx1 = (x - r + x3) / 2
-            midx2 = (x + r + x3) / 2
-            midy1 = (y + y3) / 2 + np.abs(x - x3) / 6
-            midy2 = (y + y3) / 2 - np.abs(x - x3) / 6
-            exit_coords, exit_coords_txt = None, None
-
-            if "1" in event.widget.gettags(item):
-                if x >= x2:
-                    exit_coords = (x - r, y + r / 2, midx1, midy1, x3, y3)
-                    exit_coords_txt = (midx1, midy1)
-                else:
-                    exit_coords = (x + r, y - r / 2, midx2, midy2, x3, y3)
-                    exit_coords_txt = (midx2, midy2)
-
-            elif "2" in event.widget.gettags(item):
-                if x >= x2:
-                    exit_coords = (x - r, y + r / 2, midx1, midy1, x3, y3)
-                    exit_coords_txt = (midx1, midy1)
-                else:
-                    exit_coords = (x + r, y - r / 2, midx2, midy2, x3, y3)
-                    exit_coords_txt = (midx2, midy2)
-
-            event.widget.coords(item, *exit_coords)
-            event.widget.coords(text, *exit_coords_txt)
-
-    @staticmethod
-    def move_inbound_transitions(event, name_tag, x, y, r):
-        entry_arrows = event.widget.find_withtag("to_" + name_tag)
-
-        for item in entry_arrows:
-            entry = event.widget.coords(item)
-            item_tags = event.widget.gettags(item)
-            text_tag = str((*item_tags, "text"))
-            text = event.widget.find_withtag(text_tag)
-
-            x4, y4, x5, y5, x6, y6 = entry
-            midx1 = (x4 + x - r) / 2
-            midx2 = (x4 + x + r) / 2
-            midy1 = (y + y4) / 2 - np.abs(x - x4) / 6
-            midy2 = (y + y4) / 2 + np.abs(x - x4) / 6
-
-            if x >= x4:
-                entry_coords = (x4, y4, midx1, midy1, x - r, y - r / 2)
-                entry_coords_txt = (midx1, midy1)
-            else:
-                entry_coords = (x4, y4, midx2, midy2, x + r, y + r / 2)
-                entry_coords_txt = (midx2, midy2)
-
-            event.widget.coords(item, *entry_coords)
-            event.widget.coords(text, *entry_coords_txt)
-
     def record_transition(self, transition_letters, same):
         if same:
             state = None
@@ -348,6 +343,36 @@ class InputBoard:
                     print(self.transitions)
 
 
+class Window:
+    def __init__(self, main, width, height, bg, highlightthickness, highlightbackground, highlightcolor):
+        self.container = tk.Frame(main, width=width, height=height)
+        self.canvas = tk.Canvas(self.container, width=width, height=height, bg=bg,
+                                highlightthickness=highlightthickness,
+                                highlightbackground=highlightbackground, highlightcolor=highlightcolor,
+                                scrollregion=(0, 0, 0, 0))
+
+        self.y_scrollbar = tk.Scrollbar(self.container, orient="vertical", command=self.canvas.yview, jump=1,
+                                        width=12, troughcolor="black")
+        self.scrollable_y_frame = tk.Frame(self.canvas)
+        self.canvas.create_window((0, 0), window=self.scrollable_y_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.y_scrollbar.set)
+        self.y_scrollbar.pack(side="right", fill="y")
+
+        self.x_scrollbar = tk.Scrollbar(self.container, orient="horizontal", command=self.canvas.xview, jump=1,
+                                        width=12, troughcolor="black")
+        self.scrollable_x_frame = tk.Frame(self.canvas)
+        self.canvas.create_window((0, 0), window=self.scrollable_x_frame, anchor="nw")
+        self.canvas.configure(xscrollcommand=self.x_scrollbar.set)
+        self.x_scrollbar.pack(side="bottom", fill="x")
+
+        self.width = width
+        self.height = height
+        self.font = tkFont.Font(family="consolas", size=14)
+
+    def show_pos(self, event):
+        print(event.x, event.y)
+
+
 class OutputWindow(Window):
     def __init__(self, main, width, height, bg, highlightthickness, highlightbackground, highlightcolor):
         super().__init__(main, width, height, bg, highlightthickness, highlightbackground, highlightcolor)
@@ -362,6 +387,7 @@ class OutputWindow(Window):
         self.canvas.delete("output_table")
         self.canvas.create_text(self.width / 100, self.height / 100, text=self.table_to_print, fill="black",
                                 tags="output_table", font=self.font, anchor=NW)
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'))
 
     def reset_output(self):
         reset_string = self.starting_string
@@ -418,7 +444,7 @@ class App(Frame):
         label = Entry(self)
         # label.pack(side="top", fill="x")
 
-        self.main_canvas.create_window(389, 35, anchor=NW, window=self.input_board.canvas)
+        self.main_canvas.create_window(389, 35, anchor=NW, window=self.input_board.container)
         self.main_canvas.create_window(389, 420, anchor=NW, window=self.output_board)
         self.main_canvas.create_window(17, 35, anchor=NW, window=self.input_window)
         self.main_canvas.create_window(17, 420, anchor=NW, window=self.output_window.container)
